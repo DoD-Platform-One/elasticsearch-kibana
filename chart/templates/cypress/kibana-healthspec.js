2601fb25e52a6a6de158d1b246d3ad38cdc1ec2d
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-03 12:29:09 -0400

    add helm tests

--- chart/templates/cypress/kibana-healthspec.js
+++ chart/templates/cypress/kibana-healthspec.js
@@ -2,6 +2,5 @@ describe('Basic kibana is ', function() {
   it('Check kibana is accessible', function() {
       cy.visit(Cypress.env('kibana_url'))
       cy.title().should('eq', 'Elastic')
-
   })
 })
