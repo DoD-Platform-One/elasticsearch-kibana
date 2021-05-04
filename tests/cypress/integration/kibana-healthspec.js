352e4f0ef2b5953009974c111398bcb10e2df3cd
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-03 20:04:26 -0400

    add logs for helm test

--- tests/cypress/integration/kibana-healthspec.js
+++ tests/cypress/integration/kibana-healthspec.js
@@ -1,6 +1,6 @@
 describe('Basic kibana is ', function() {
   it('Check kibana is accessible', function() {
-      cy.visit('http://elasticsearch-kibana-kb-http:5601')
+      cy.visit('kibana.bigbang.dev'')
       cy.title().should('eq', 'Elastic')
 
   })
