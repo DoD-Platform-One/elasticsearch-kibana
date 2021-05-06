1ca549c2e2b848475809d40178961f961ab4cc34
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-06 08:24:16 -0400

    Add helm test

--- chart/cypress/kibana-health.spec.js
+++ chart/cypress/kibana-health.spec.js
@@ -1,25 +1,7 @@
 describe('Basic Kibana', function() {
   it('Check Kibana UI is accessible', function() {
-    cy.visit('https://localhost:5601')
+    cy.visit('https://elasticsearch-kibana-kb-http:5601')
     cy.title().should('eq', 'Elastic')
-    cy.get('[name="username"]')
-        .type('elastic')
-        .should('have.value', 'elastic');
-
-      // Fill the password
-      cy.get('[name="password"]')
-        .type(Cypress.env('password'))
-        .should('have.value', Cypress.env('password'));
-
-      // Locate and submit the form
-      cy.get('form').submit();
-      
-      // Verify the app redirected you to the homepage
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/app/home');
-      
-      // Verify the page title is "Home"
-     cy.title().should('eq', 'Home - Elastic');
-
-  
+    
   })
 })
