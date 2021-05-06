dff4848f511a5901411131255af2cc8270ebbfd7
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-06 11:37:08 -0400

    add helm test

--- chart/cypress/kibana-health.spec.js
+++ chart/cypress/kibana-health.spec.js
@@ -1,8 +1,9 @@
 describe('Basic Kibana', function() {
   it('Check Kibana UI is accessible', function() {
-    cy.visit( 'https://elasticsearch-kibana-kb-http:5601/login?next=%2F') 
+    //cy.visit( 'https://elasticsearch-kibana-kb-http:5601/login?next=%2F') 
+    cy.visit(Cypress.env('url')+"/login?next=%2F")
     cy.title().should('eq', 'Elastic')
-    cy.get('input[name="username"]')
+    cy.get('[name="username"]')
     .type('elastic')
     .should('have.value', 'elastic');
     
--- tests/cypress/integration/kibana-healthspec.js
+++ tests/cypress/integration/kibana-healthspec.js
@@ -2,6 +2,9 @@ describe('Basic kibana is ', function() {
   it('Check kibana is accessible', function() {
       cy.visit('kibana.bigbang.dev')
       cy.title().should('eq', 'Elastic')
+      cy.get('[name="username"]')
+      .type('elastic')
+      .should('have.value', 'elastic');
 
   })
 })
