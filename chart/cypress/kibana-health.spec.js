fc3f6912cf244398ad2f17e269b6b589963157b1
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-06 14:37:24 -0400

    add helm test

--- chart/cypress/kibana-health.spec.js
+++ chart/cypress/kibana-health.spec.js
@@ -1,12 +1,33 @@
-describe('Basic Kibana', function() {
-  it('Check Kibana UI is accessible', function() {
-    //cy.visit( 'https://elasticsearch-kibana-kb-http:5601/login?next=%2F') 
-    cy.visit(Cypress.env('url')+"/login?next=%2F")
-    cy.title().should('eq', 'Elastic')
-    cy.get('[name="username"]')
-    .type('elastic')
-    .should('have.value', 'elastic');
-    
-    
-  })
-})
+describe("Basic Kibana", function () {
+  it("Check Kibana UI is accessible", function () {
+    //cy.visit( 'https://elasticsearch-kibana-kb-http:5601/login?next=%2F')
+    cy.visit(Cypress.env("url") + "/login?next=%2F");
+    cy.title().should("eq", "Elastic");
+    cy.get("input[name=username]", { timeout: 15000 })
+      .should("be.visible")
+      .type("elastic")
+      .should("have.value", "elastic");
+    cy.get("input[name=password]")
+      .type(Cypress.env("password"))
+      .should("have.value", Cypress.env("password"));
+
+    cy.get("form").submit();
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
+
+    // Verify the app redirected you to the homepage
+    cy.location("pathname", { timeout: 10000 }).should("eq", "/app/home");
+
+    /*cy.contains("Try our sample data", { timeout: 15000 })
+      .should("be.visible")
+      .click();
+    cy.get("button.euiHeaderSectionItem__button", { timeout: 15000 })
+      .first()
+      .click();
+    cy.contains("Discover", { timeout: 15000 }).click();
+    cy.title().should("eq", "Index patterns - Elastic"); */
+  });
+});
--- tests/cypress/integration/kibana-healthspec.js
+++ tests/cypress/integration/kibana-healthspec.js
@@ -1,10 +1,28 @@
-describe('Basic kibana is ', function() {
-  it('Check kibana is accessible', function() {
-      cy.visit('kibana.bigbang.dev')
-      cy.title().should('eq', 'Elastic')
-      cy.get('[name="username"]')
-      .type('elastic')
-      .should('have.value', 'elastic');
+describe("Basic kibana is ", function () {
+  it("Check kibana is accessible", function () {
+    cy.visit("https://localhost:5601/login?next=%2F");
+    cy.title().should("eq", "Elastic");
 
-  })
-})
+   /* cy.get("input[name=username]", { timeout: 15000 })
+      .should("be.visible")
+      .type("elastic")
+      .should("have.value", "elastic");
+    cy.get("input[name=password]", { timeout: 15000 })
+      .should("be.visible")
+      .type("7hc8Q47mp2hROO4Luh9w6i41");
+    cy.get("form").submit();
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
+    // Verify the app redirected you to the homepage
+    cy.location("pathname", { timeout: 10000 }).should("eq", "/app/home");
+    //cy.contains("Try our sample data", { timeout: 15000 }) .click();
+    cy.get("button.euiHeaderSectionItem__button", { timeout: 15000 })
+      .first()
+      .click();
+    cy.contains("Discover", { timeout: 15000 }).click();
+    cy.title().should("eq", "Index patterns - Elastic"); */
+  });
+});
