cd78d1b4cc6cd2b63b563e1e09207b7a0feee3c4
Kavitha Thulasiraman <kavithathulasiraman@Kavithas-MacBook-Pro-2.local>
2021-05-06 14:44:21 -0400

    add helm test

--- tests/cypress/integration/kibana-healthspec.js
+++ tests/cypress/integration/kibana-healthspec.js
@@ -1,6 +1,6 @@
 describe("Basic kibana is ", function () {
   it("Check kibana is accessible", function () {
-    cy.visit("https://localhost:5601/login?next=%2F");
+    cy.visit("https://kibana.bigbang.dev:5601/login?next=%2F");
     cy.title().should("eq", "Elastic");
 
    /* cy.get("input[name=username]", { timeout: 15000 })
