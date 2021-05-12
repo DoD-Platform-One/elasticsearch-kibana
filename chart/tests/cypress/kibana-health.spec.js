6f051c0863c46046f36423a510cb25de4d12464f
garcia.ryan <garcia.ryan@solute.us>
2021-05-12 13:51:30 -0600

    enhancing kibana ui cypress tests

--- chart/tests/cypress/kibana-health.spec.js
+++ chart/tests/cypress/kibana-health.spec.js
@@ -7,22 +7,17 @@ describe("Basic Kibana", function () {
       .type("elastic")
     cy.get('input[name="password"]').type(Cypress.env('elastic_password'))
     cy.get('button[type="submit"]').click();
-    Cypress.on("uncaught:exception", (err, runnable) => {
-      // returning false here prevents Cypress from
-      // failing the test
-      return false;
-    });
 
     // Verify the app redirected you to the homepage
     cy.location("pathname", { timeout: 10000 }).should("eq", "/app/home");
 
-    /*cy.contains("Try our sample data", { timeout: 15000 })
+    cy.contains("Try our sample data", { timeout: 15000 })
       .should("be.visible")
       .click();
     cy.get("button.euiHeaderSectionItem__button", { timeout: 15000 })
       .first()
       .click();
     cy.contains("Discover", { timeout: 15000 }).click();
-    cy.title().should("eq", "Index patterns - Elastic"); */
+    cy.title().should("eq", "Index patterns - Elastic");
   });
 });
