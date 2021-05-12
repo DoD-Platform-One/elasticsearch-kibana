d4fc05acea6bea5ef012691ce089f31eaa658701
garcia.ryan <garcia.ryan@solute.us>
2021-05-12 10:49:21 -0600

    Updating kibana cypress spec 2

--- chart/tests/cypress/kibana-health.spec.js
+++ chart/tests/cypress/kibana-health.spec.js
@@ -1,6 +1,5 @@
 describe("Basic Kibana", function () {
   it("Check Kibana UI is accessible", function () {
-    //cy.visit( 'https://elasticsearch-kibana-kb-http:5601/login?next=%2F')
     cy.visit(`${Cypress.env('kibana_url')}/login?next=%2F`);
     cy.title().should("eq", "Elastic");
     cy.get("input[name=username]", { timeout: 15000 })
