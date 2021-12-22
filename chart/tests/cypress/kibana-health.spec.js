42d9b55334af97bc9ebbd922044ebef7fb74be60
Brendon Lloyd <blloyd@revacomm.com>
2021-12-22 19:36:33 +0000

    Verify that log records exist during CI process

--- chart/tests/cypress/kibana-health.spec.js
+++ chart/tests/cypress/kibana-health.spec.js
@@ -1,26 +1,41 @@
-describe("Basic Kibana", function () {
-  it("Log into Kibana", function () {
+describe("Log in and look for logs in Kibana", function () {
+  // required otherwise the tests will error out immediately when it sees there isn't an
+  // active session
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
+  function kibana_login () {
     cy.visit(Cypress.env('kibana_url'))
     cy.title().should("eq", "Elastic")
     cy.wait(6000)
-    cy.get('input[name="username"]', { timeout: 15000 })
+    cy.get('input[name="username"]', { timeout: 30000 })
       .type("elastic")
     cy.get('input[name="password"]').type(Cypress.env('elastic_password'))
-    cy.get('button[type="submit"]').click();
-
-    // Verify the app redirected you to the homepage
-    cy.location("pathname", { timeout: 10000 }).should("eq", "/app/home");
-  });
-  //Going any further leads to Kibana not loading properly, probably because of browser within Cypress, shows "Kibana did not load properly. Check the server output for more information"
-  /*it("Visit Discover Page", function () {
-    cy.contains("Add sample data", { timeout: 15000 })
-      .should("be.visible")
-      .click()
-    cy.get("button.euiHeaderSectionItem__button", { timeout: 15000 })
-      .first()
-      .click()
-    cy.get
-    cy.get('button[id="130fa6f1-b35f-11eb-a21c-079e4de1ead6Title"]', { timeout: 15000 }).click();
-    cy.title().should("eq", "Discover - Elastic");
-  });*/
-});
+    cy.get('button[type="submit"]').click()
+    cy.location("pathname", { timeout: 10000 }).should("eq", "/app/home")
+    cy.task('log', 'successfully logged in...')
+    }
+  // save the cookies in order to have multiple tests with one login
+  beforeEach(function () {
+    cy.getCookies().then(cookies => {
+      const namesOfCookies = cookies.map(cm => cm.name)
+      Cypress.Cookies.preserveOnce(...namesOfCookies)
+    })
+  })
+  before(function () {
+    kibana_login() 
+  })
+  it("Log into Kibana", function () {
+    // note: all of the work is being done by the before function, which is in turn
+    // done by kibana_login()
+    cy.contains('Welcome to Elastic')
+  })
+  if (Cypress.env("expect_logs")) {
+    it( "Check for logs", function () {
+      cy.task('log', 'checking for logs in kibana...')
+      cy.visit(Cypress.env('kibana_url') + '/app/management/data/index_management/indices', { timeout: 90000 })
+      cy.get('th.euiTableRowCell', { timeout: 90000 }).its('length').should('be.gte', 1)
+      cy.task('log', 'log entries detected...')
+    })
+  }
+})
